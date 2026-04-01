c8d1aba71890af2c8bbd87cfbdbda24bec2f046a
preeti-myana <peeti2412@gmail.com>
2026-04-01 15:19:22 +0530

    Day 1: Extracted title and price

--- scraper.py
+++ scraper.py
@@ -1,13 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://books.toscrape.com/"
+url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
+# soup.find(TAG, ATTRIBUTE)
+title = soup.find("h1")
+print("Title:",title.text)
+
+price = soup.find("p", class_= "price_color")
+print(f"Price: {price.text}")
+
+
 # print(response)
 # print(response.text)
-
-print(soup.prettify())
+# print(soup.prettify())
